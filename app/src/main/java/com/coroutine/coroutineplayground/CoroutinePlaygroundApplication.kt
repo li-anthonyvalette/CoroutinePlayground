8b748007dbfc59d139a552e30bfb5986083437f9
anthony.valette <anthony.valette@groupeseloger.com>
2022-08-31 14:34:53 +0200

    wip

---
+++ app/src/main/java/com/coroutine/coroutineplayground/CoroutinePlaygroundApplication.kt
@@ -0,0 +1,8 @@
+package com.coroutine.coroutineplayground
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CoroutinePlaygroundApplication: Application() {
+}
